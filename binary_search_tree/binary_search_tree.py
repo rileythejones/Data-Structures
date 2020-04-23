b81bba0384ae2275fd9428403389c9b24b904ffc
rileythejones <rileymjones@gmail.com>
2020-04-23 16:21:28 -0600

    binary searh day 2

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('../queue_and_stack')
 from dll_queue import Queue
 from dll_stack import Stack
-
+from collections import deque
 
 class BinarySearchTree:
     def __init__(self, value):
@@ -24,8 +24,6 @@ class BinarySearchTree:
             else:
                 self.right = BinarySearchTree(value)
 
-    # Return True if the tree contains the value
-    # False if it does not 
     def contains(self, target):
         if self.value is target:
             return True
@@ -38,71 +36,69 @@ class BinarySearchTree:
             if self.left is None:
                 return False
             else:
-                return BinarySearchTree.contains(self.left, target)
+                return BinarySearchTree.contains(self.left, target) 
 
-    
-#     def contains(self, target):
-#         if self.value is target:
-#             print("Check Node")
-#             return True 
-#         elif self.left:
-#             print("Check Left")
-#             if self.left.value is target:
-#                 print("Check Left Node")
-#                 return True
-#             elif self.left.contains(target):
-#                 print("Call contains on Left Node")
-#                 return True
-#             elif self.right:
-#                 print("Check Right within Left")
-#                 if self.right.contains(target):
-#                     print("Call contains on Right Node within Left Node")
-#                     return True
-
-#         elif self.right:
-#             print("Check Right")
-#             if self.right.value is target:
-#                 print("Check Right Node")
-#                 return True
-#             elif self.right.contains(target):
-#                 print("Call contains on Right Node")
-#                 return True
-#             elif self.left: 
-#                 print("Check Left within Right")
-#                 if self.left.contains(target):
-#                     print("Call contains on Left Node within Right Node")
-#                     return True
-#         else:
-#             return False 
-#             print("Return when not found")
-#             return False 
-
-
-    # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        node = self
+        while node.right:
+            node = node.right
+        return node.value
 
-    # Call the function `cb` on the value of each node
-    # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+
+    def iterative_for_each(self, cb):
+        stack = []
+
+        stack.append(self)
+        while len(stack) > 0:
+            current_node = stack.pop()
+            if current_node.right:
+                stack.append(current_node.right)
+            if current_node.left:
+                stack.append(current_node.left)
+            cb(current_node.value)
+
+    def breadth_first_iterative_for_each(self, cb):
+        # depth-first : stack
+        # breadth-first: queue
+
+        q = deque()
+        q.append(self)
+
+        while len(q) > 0:
+            current_node = q.popleft()
+            if current_node.left:
+                q.append(current_node.left)
+            if current_node.right:
+                q.append(current_node.right)
+            cb(current_node.value)
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if node.left:
+            node.left.for_each(print)
+        print(node.value)
+        if node.right:
+            node.right.for_each(print)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        node.breadth_first_iterative_for_each(print)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        node.iterative_for_each(print)
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
